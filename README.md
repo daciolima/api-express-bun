008a360215d66397f1a332ae594deca0c9866aa4
daciolima <contato.dacio@gmail.com>
2023-10-07 12:03:45 -0300

    implementação GraphQL

--- README.md
+++ README.md
@@ -12,3 +12,46 @@ bun install
 - Controllers => Recebe os parâmetros da requisição, fazer as validações da entrada dos parâmetros e depois responder para o router.
 - Services => Trata da regra de negório(CRUD) e caso necessário consulta os repository e devolve para os controllers.
 - Repositories => Trata da persistência dos dados e devolve confirmação para os services.
+
+
+
+### Consulta GraphQL. Interface web
+```shell
+# mutation {
+#   createAccount(account: {
+#     name: "Carla Silva",
+#     balance: 1500
+#   })
+#   {
+#     id
+#     name
+#     balance
+#   }
+  
+#   updateAccount(account: {
+#     id: 15
+#     name: "Vanessa Mota",
+#     balance: 3200
+#   })
+#   {
+#     id
+#     name
+#     balance
+#   }
+  
+#   deleteAccount(id: 16)
+# }
+
+# {	
+#   getAccount(id: 5){
+#   	id
+#     name
+#     balance
+# 	}
+#   getAccounts {
+#     id
+#     name
+#     balance
+#   }
+# }
+```
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import winston from 'winston';
 import accountsRouter from './routes/accountRoutes.js'
 import {promises as fs} from 'fs';
 import cors from 'cors';
+import { buildSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
+import accountService from './services/accountService.js';
 // import swaggerUi from 'swagger-ui-express';
 // import {swaggerDocuments} from './docs.js'; 
 
@@ -42,11 +45,61 @@ app.use(cors());
 // Documentação Swagger
 // app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocuments));
 
+
 // Setando entrada de route específica
 app.use("/account", accountsRouter);
 
 // Serve static
 app.use(express.static("public"))
+
+
+// Definindo schema GraphQL para um tipo e suas querys e mutations. Nesse caso para Account
+// Type Account trata os Gets. Input InputAccount trata o Create, Update e Delete.
+const schemaAccount = buildSchema(`
+    type Account{ 
+        id: Int
+        name: String
+        balance: Float
+    }
+    input InputAccount { 
+        id: Int
+        name: String
+        balance: Float
+    }
+    type Query {
+        getAccounts: [Account]
+        getAccount(id: Int): Account
+    }
+    type Mutation {
+        createAccount(account: InputAccount): Account 
+        updateAccount(account: InputAccount): Account
+        deleteAccount(id: Int): Boolean
+    }
+`)
+
+const rootAccount = {
+    getAccounts: () => accountService.getAllAccount(),
+    getAccount(args){
+        return accountService.getOneAccount(args.id)
+    },
+    createAccount({account}) {
+        return accountService.createAccount(account)
+    },
+    updateAccount({account}) {
+        return accountService.putAccount(account)
+    },
+    deleteAccount(args) {
+        return accountService.deleteAccount(args.id)
+    }     
+}
+
+// Definindo middleware para o GraphQL
+app.use('/graphql', graphqlHTTP({
+    schema: schemaAccount, // Setar o Schema account
+    rootValue: rootAccount, // Resolve da query. O que deve ser feito quando chegar requisição para o Schema.
+    graphiql: true // Carregar a interface gráfica do GraphQL
+}))
+
 
 // Config server
 app.listen(3000, async () => {
